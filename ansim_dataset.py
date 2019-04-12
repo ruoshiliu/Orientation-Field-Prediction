f188a72de3d79cf228c9c86d59b1798a62b5d552
Ruoshi Liu <ruoshiliu@brandeis.edu>
2019-04-12 02:29:35 -0400

    train notebook

--- ansim_dataset.py
+++ ansim_dataset.py
@@ -11,6 +11,22 @@ import math
 import random
 import os
 
+def create_circular_mask(h, w, center=None, radius=None):
+
+    if center is None: # use the middle of the image
+        center = [int(w/2), int(h/2)]
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = dist_from_center <= radius
+    mask = mask.astype(int)
+    return mask
+
+mask = create_circular_mask(128,128)
+
 class ansimDataset(Dataset):
     """Face Landmarks dataset."""
 
